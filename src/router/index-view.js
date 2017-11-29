708bc71cb8cf5d09625a2fa832a2b42f9221f7c4
ValeriyKobysh <vmksocnet@outlook.com>
2017-11-29 16:51:51 +0200

    Update

--- src/router/index-view.js
+++ src/router/index-view.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Meta from 'vue-meta';
 
 Vue.use(VueRouter);
+Vue.use(Meta)
 // import pages
 import indexPage from '../view/Index'
 import authPage from '../view/Auth/Auth'
